cfae0a5e4fa41316bd174ae0ed9da3eab3e51e80
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-03 15:02:55 +0800

    add component counter

---
+++ src/Components/Counter/index.jsx
@@ -0,0 +1,27 @@
+import  React  from 'react';
+
+class Counter extends React.Component{
+constructor(props){
+    super(props)
+    this.state={
+        number: 0
+    }
+}
+ onClickAdd = () =>{
+    this.setState({number: this.state.number+1})
+}
+onClickDelete = () =>{
+    this.setState({number: this.state.number-1})
+}
+render(){
+    return (
+        <div>
+            <button onClick={this.onClickAdd}>+</button>
+            <mark>{this.state.number}</mark>
+            <button onClick={this.onClickDelete}>-</button>
+        </div>
+        )
+       
+}
+}
+export default Counter
