60b6f93920ea870099e7d6abe2c2e20471658af9
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-03 15:27:06 +0800

    [Counter][James] refactor: format the code

--- src/Components/Counter/index.jsx
+++ src/Components/Counter/index.jsx
@@ -1,27 +1,27 @@
-import  React  from 'react';
+import React from 'react';
 
-class Counter extends React.Component{
-constructor(props){
-    super(props)
-    this.state={
-        number: 0
+class Counter extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            number: 0
+        }
     }
-}
- onClickAdd = () =>{
-    this.setState({number: this.state.number+1})
-}
-onClickDelete = () =>{
-    this.setState({number: this.state.number-1})
-}
-render(){
-    return (
-        <div>
-            <button onClick={this.onClickAdd}>+</button>
-            <mark>{this.state.number}</mark>
-            <button onClick={this.onClickDelete}>-</button>
-        </div>
+    onClickAdd = () => {
+        this.setState({ number: this.state.number + 1 })
+    }
+    onClickDelete = () => {
+        this.setState({ number: this.state.number - 1 })
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.onClickAdd}>+</button>
+                <mark>{this.state.number}</mark>
+                <button onClick={this.onClickDelete}>-</button>
+            </div>
         )
-       
-}
+
+    }
 }
 export default Counter
